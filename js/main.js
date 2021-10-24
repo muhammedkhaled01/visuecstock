4a01d34e650905af3701aef10c22fe84721b464e
Mohamed Khaled <mohamedkhaled667.mk@gmail.com>
2021-10-24 13:33:40 +0200

    end the arabic V

--- js/main.js
+++ js/main.js
@@ -1,62 +1,16 @@
 $(document).ready(function () {
-    $('.header').height($(window).height());
+
+    $('.header-enterprise').height($(window).height());
     $(window).resize(function () {
-        $('.header').height($(window).height());
+        $('.header-enterprise').height($(window).height());
 
     })
-
-    var owl = $('.owl-carousel');
-    owl.owlCarousel({
-        items: 5,
-        loop: true,
-        margin: 10,
-        autoplay: true,
-        autoplayTimeout: 1500,
-        autoplayHoverPause: true
+    $(".check-tap .item").click(function () {
+        $(this).siblings().removeClass('active');
+        $(this).addClass('active');
+        // $(this).addClass("active").siblings('item').removeClass("active");
+        // console.log("hi");
     });
-    $('.owl-carousel').owlCarousel({
-        loop: true,
-        margin: 10,
-        responsiveClass: true,
-        responsive: {
-            0: {
-                items: 1,
-                nav: true
-            },
-            600: {
-                items: 3,
-                nav: false
-            },
-            1000: {
-                items: 5,
-                nav: true,
-                loop: false
-            }
-        }
-    })
-    $(".owl-carousel").owlCarousel();
-    $('.owl-carousel').owlCarousel({
-        loop: true,
-        margin: 10,
-        responsiveClass: true,
-
-        responsive: {
-            0: {
-                items: 1,
-                nav: true
-            },
-            600: {
-                items: 3,
-                nav: false
-            },
-            1000: {
-                items: 5,
-                nav: true,
-                loop: false
-            }
-        }
-    })
-
 
 });
 
